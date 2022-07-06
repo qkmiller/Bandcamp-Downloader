93365f558351a5456eac32b71142d2ccccebbc81
qkmiller <millerquinn66@gmail.com>
2022-07-05 21:30:55 -0700

    Remove unnecessary bandcampdl.py file

--- BandcampDL.py
+++ BandcampDL.py
@@ -157,3 +157,24 @@ class BandcampDL():
         if re.match(fmt_str.format(urlchars, urlchars), url):
             return True
         return False
+
+
+if __name__ == '__main__':
+    import sys
+
+    bcdl = BandcampDL()
+
+    def usage():
+        print("Bandcamp album downloader")
+        print("Usage:")
+        print("./python3 bandcampdl.py [album url]")
+        print("./python3 bandcampdl.py -f [file]")
+
+    if (len(sys.argv) < 2 or len(sys.argv) > 3):
+        usage()
+    elif (sys.argv[1] == '-h'):
+        usage()
+    elif (sys.argv[1] == '-f'):
+        bcdl.get_from_file(sys.argv[2])
+    else:
+        bcdl.get_from_url(sys.argv[1])
